7dc1b45a5fad8fa67900e1849f2231f917c05b38
Merentu <vihlygorg@gmail.com>
2017-01-16 22:32:05 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+var pass = 'qwerty';
+var ok = 'pash2.html';
+var error = 'Доступ запрещен!';
+passwd.addEventListener('keyup', joinValues, false);
+function enter()
+{
+if(passwd.value==pass)
+{location.href=ok;}
+else{
+document.write(error);
+}}
